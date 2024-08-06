be07ed43ce2a64b07e91a15b19b149296eb7dcc5
Program <ProgramCX@Outlook.com>
2024-08-06 23:33:09 +0800

    播放音乐（使用外部的jquery）

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-$.
+﻿// document.getElementById('playbutton').addEventListener('click',()=>{
+//     console.log("被点击了");
+// })
+let autoplay=true;
+
+function playAndPause(){
+    if(!playState){
+        $('#playbutton').text('停止播放');
+        document.getElementById('music').play();
+     }else{
+        $('#playbutton').text('开始播放');
+        document.getElementById('music').pause();
+        document.getElementById('music').currentTime=0;
+       
+     }
+     playState=!playState;
+}
+$(document).ready(function(){  
+//    playAndPause();
+});
+let playState=false;
+$('#playbutton').click(function() {  
+     playAndPause();
+  });  
